558a60745cef92a9800a45e335d93952dcf6a1e6
Maria Ferreira <maria.ferreira312017@gmail.com>
2025-07-11 12:00:17 +0800

    Fix CORS and add data loader

--- src/main/java/com/alura/finalProject/randomPhrase/controller/PhraseController.java
+++ src/main/java/com/alura/finalProject/randomPhrase/controller/PhraseController.java
@@ -4,17 +4,24 @@ import com.alura.finalProject.randomPhrase.DTO.PhrasesDTO;
 import com.alura.finalProject.randomPhrase.service.PhraseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/phrases") // Adicione esta linha
 public class PhraseController {
 
     @Autowired
     private PhraseService service;
 
-    @GetMapping("/series/frases")
+    @GetMapping("/random") // Agora será /api/phrases/random
     public PhrasesDTO receivingRandomPhrase(){
         return service.receivingRandomPhrase();
+    }
+
+    @GetMapping("/") // Para listar todas (opcional)
+    public String listPhrases(){
+        return "Endpoint para listar frases";
     }
 
 }
