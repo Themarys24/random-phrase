dff743e0ac135292f28843e194695f4f73910a48
Maria Ferreira <maria.ferreira312017@gmail.com>
2025-07-11 11:51:39 +0800

    Add home controller

---
+++ src/main/java/com/alura/finalProject/randomPhrase/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.alura.finalProject.randomPhrase.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Random Phrase API está funcionando! ✅<br>" +
+                "Endpoints disponíveis:<br>" +
+                "- GET /api/phrases (listar frases)<br>" +
+                "- POST /api/phrases (criar frase)<br>" +
+                "- GET /api/phrases/random (frase aleatória)";
+    }
+}
